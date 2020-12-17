5b4efde7bfdac94cd3edbf14e1b6fac25068a916
Sudhir Bhandary <bhandarys@yahoo.com>
2020-12-17 20:55:18 +0530

    Trying to create a editor like w3schools. Created 2 panes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var numbers = [175, 50, 25];
 
-document.getElementById("demo").innerHTML = numbers.reduce(myFunc).toString();
+document.getElementById("place-holder").innerHTML = numbers.reduce(myFunc).toString();
 
 function myFunc(total, num) {
   return total - num;
